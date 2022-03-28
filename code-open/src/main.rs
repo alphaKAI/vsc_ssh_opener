d1cbc73c9be28d503e0ef4a4a998e35f86b77c56
azarashi <46126763+azarashi2931@users.noreply.github.com>
2022-03-29 02:21:05 +0900

    Introduce clap derive to code-open

--- code-open/src/main.rs
+++ code-open/src/main.rs
@@ -1,9 +1,24 @@
-use clap::{Arg, Command};
+use clap::Parser;
 use code_open_common::*;
 use path_absolutize::*;
 use std::io::Write;
 use std::path::MAIN_SEPARATOR;
 use std::{env, net::TcpStream, path::PathBuf};
+
+/// open VSCode over SSH
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// ip address of the server to be connected
+    #[clap(short, long, default_value_t = DEFAULT_IP.to_string())]
+    ip: String,
+
+    /// port number of the server to be connected
+    #[clap(short, long, default_value_t = DEFAULT_PORT)]
+    port: u16,
+
+    path: Option<String>,
+}
 
 fn send_request_to_server(code_open_config: &CodeOpenConfig, code_open_req: CodeOpenRequest) {
     let mut connection = TcpStream::connect((code_open_config.ip.as_str(), code_open_config.port))
@@ -32,52 +47,20 @@ fn main() {
     let ssh_flag = env::vars().any(|(k, _)| k == "SSH_CONNECTION");
     let mut code_open_config = CodeOpenConfig::default();
 
-    let default_port_str = &DEFAULT_PORT.to_string();
-
-    let app = Command::new("code-open")
-        .version("0.1.0")
-        .author("Akihiro Shoji <alpha.kai.net@alpha-kai-net.info>")
-        .about("open VSCode over SSH")
-        .arg(
-            Arg::new("ip")
-                .required(false)
-                .short('i')
-                .long("ip")
-                .takes_value(true)
-                .default_value(DEFAULT_IP)
-                .help("ip address of the server to be connected"),
-        )
-        .arg(
-            Arg::new("port")
-                .required(false)
-                .short('p')
-                .long("port")
-                .takes_value(true)
-                .default_value(default_port_str)
-                .help("port number of the server to be connected"),
-        )
-        .arg(Arg::new("path").required(false));
-
-    let matches = app.get_matches();
-
-    if let Some(ip) = matches.value_of("ip") {
-        code_open_config.set_ip(ip.to_owned());
-    }
-
-    if let Some(port) = matches.value_of("port") {
-        code_open_config.set_port(port.parse().expect("failed to parse given port number"));
-    }
+    let args = Args::parse();
+    code_open_config.set_ip(args.ip);
+    code_open_config.set_port(args.port);
 
     if !ssh_flag {
         println!("Error this command should be executed in SSH");
         return;
     }
 
-    let path = matches.value_of("path").map_or_else(
+    let path = args.path.map_or_else(
         || env::current_dir().unwrap(),
         |path| {
             PathBuf::from(
-                shellexpand::full(path)
+                shellexpand::full(&path)
                     .expect("failed to tilde expad")
                     .to_string(),
             )
